3ffb0322c95667d54224972547930926c94ee57d
EmilCosma <109912838+EmilCosma@users.noreply.github.com>
2024-11-25 14:36:13 +0200

    Earthquake sound effect added

--- VR Natural Disaster Simulator/Assets/Scripts/CameraShake.cs
+++ VR Natural Disaster Simulator/Assets/Scripts/CameraShake.cs
@@ -4,11 +4,12 @@ public class CameraShake : MonoBehaviour
 {
     public float shakeDuration = 5.0f;  // Durata cutremurului la apăsarea Enter (5 secunde)
     public float shakeMagnitude = 0.1f; // Puterea cutremurului
-
     public Camera cameraToShake; // Camera pe care să o miște
+    public AudioClip shakeSound; // Sunetul cutremurului
 
     private Vector3 initialPosition;
     private float shakeTimeRemaining;
+    public AudioSource audioSource;
 
     private void Start()
     {
@@ -16,11 +17,15 @@ public class CameraShake : MonoBehaviour
         {
             cameraToShake = GetComponent<Camera>();
         }
-        
+
         if (cameraToShake != null)
         {
             initialPosition = cameraToShake.transform.localPosition;
         }
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = shakeSound;
+        audioSource.playOnAwake = false; // Prevent the sound from playing automatically
     }
 
     private void Update()
@@ -46,5 +51,6 @@ public class CameraShake : MonoBehaviour
         shakeDuration = duration;
         shakeMagnitude = magnitude;
         shakeTimeRemaining = shakeDuration;
+        audioSource.Play(); // Play the shake sound
     }
 }
