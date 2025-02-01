dcc387ed6b8f18bed522860952fee0518c8598ec
AdelinCojocaruA2 <cojocaruadelin@gmail.com>
2025-02-01 03:28:58 +0200

    tutorial added

--- VR Natural Disaster Simulator/Assets/Scripts/MenuInteract.cs
+++ VR Natural Disaster Simulator/Assets/Scripts/MenuInteract.cs
@@ -8,25 +8,40 @@ public class MenuInteract : MonoBehaviour
 {
     public GameObject startMenu;
     public GameObject levelSelect;
+    public GameObject earthquakeTutorial;
+    public GameObject[] earthquakeTutorialPages;
+    public GameObject fireTutorial;
+    public GameObject[] fireTutorialPages;
     public GameObject arrowPointer;
     public GameObject playButton;
+    public GameObject leftFireButton;
+    public GameObject rightFireButton;
+    public GameObject leftEarthquakeButton;
+    public GameObject rightEarthquakeButton;
     private Canvas startMenuCanvas;
     private Canvas levelSelectCanvas;
+    private Canvas earthquakeTutorialCanvas;
+    private Canvas fireTutorialCanvas;
     private bool levelWasSelected;
     private string currentDifficulty;
     private string currentLevel;
     private Transform buttonTransform;
     private AudioSource audioSource;
+    private int currentPage = 0;
 
     void Start()
     {
         startMenuCanvas = startMenu.GetComponent<Canvas>();
         levelSelectCanvas = levelSelect.GetComponent<Canvas>();
+        earthquakeTutorialCanvas = earthquakeTutorial.GetComponent<Canvas>();
+        fireTutorialCanvas = fireTutorial.GetComponent<Canvas>();
 
         audioSource = GetComponent<AudioSource>();
 
         startMenuCanvas.enabled = true;
         levelSelectCanvas.enabled = false;
+        earthquakeTutorialCanvas.enabled = false;
+        fireTutorialCanvas.enabled = false;
 
         arrowPointer.SetActive(false);
         playButton.SetActive(false);
@@ -37,6 +52,100 @@ public class MenuInteract : MonoBehaviour
     {
         startMenuCanvas.enabled = false;
         levelSelectCanvas.enabled = true;
+    }
+
+    public void showEarthquakeTutorial()
+    {
+        levelSelectCanvas.enabled = false;
+        earthquakeTutorialCanvas.enabled = true;
+        earthquakeTutorialPages[0].SetActive(true);
+        for(int i = 1; i < earthquakeTutorialPages.Length; i++)
+        {
+            earthquakeTutorialPages[i].SetActive(false);
+        }
+        leftEarthquakeButton.SetActive(false);
+        rightEarthquakeButton.SetActive(true);
+    }
+
+    public void turnLeftEarthquake()
+    {
+        earthquakeTutorialPages[currentPage].SetActive(false);
+        currentPage--;
+        earthquakeTutorialPages[currentPage].SetActive(true);
+        if(currentPage == 0)
+        {
+            leftEarthquakeButton.SetActive(false);
+        }
+        if(currentPage != earthquakeTutorialPages.Length - 1 && !rightEarthquakeButton.activeSelf)
+        {
+            rightEarthquakeButton.SetActive(true);
+        }
+    }
+    
+    public void turnRightEarthquake()
+    {
+        earthquakeTutorialPages[currentPage].SetActive(false);
+        currentPage++;
+        earthquakeTutorialPages[currentPage].SetActive(true);
+        if(currentPage == earthquakeTutorialPages.Length - 1)
+        {
+            rightEarthquakeButton.SetActive(false);
+        }
+        if(currentPage != 0 && !leftEarthquakeButton.activeSelf)
+        {
+            leftEarthquakeButton.SetActive(true);
+        }
+    }
+
+    public void showFireTutorial()
+    {
+        levelSelectCanvas.enabled = false;
+        fireTutorialCanvas.enabled = true;
+        fireTutorialPages[0].SetActive(true);
+        for(int i = 1; i < fireTutorialPages.Length; i++)
+        {
+            fireTutorialPages[i].SetActive(false);
+        }
+        leftFireButton.SetActive(false);
+        rightFireButton.SetActive(true);
+    }
+
+    public void turnLeftFire()
+    {
+        fireTutorialPages[currentPage].SetActive(false);
+        currentPage--;
+        fireTutorialPages[currentPage].SetActive(true);
+        if(currentPage == 0)
+        {
+            leftFireButton.SetActive(false);
+        }
+        if(currentPage != fireTutorialPages.Length - 1 && !rightFireButton.activeSelf)
+        {
+            rightFireButton.SetActive(true);
+        }
+    }
+    
+    public void turnRightFire()
+    {
+        fireTutorialPages[currentPage].SetActive(false);
+        currentPage++;
+        fireTutorialPages[currentPage].SetActive(true);
+        if(currentPage == fireTutorialPages.Length - 1)
+        {
+            rightFireButton.SetActive(false);
+        }
+        if(currentPage != 0 && !leftFireButton.activeSelf)
+        {
+            leftFireButton.SetActive(true);
+        }
+    }
+
+    public void closeTutorial()
+    {
+        levelSelectCanvas.enabled = true;
+        earthquakeTutorialCanvas.enabled = false;
+        fireTutorialCanvas.enabled = false;
+        currentPage = 0;
     }
 
     public void getButtonPosition(Transform buttonPos)
